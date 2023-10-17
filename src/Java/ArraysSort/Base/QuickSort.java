610439375af4a8c0c8ec4b1ed3300ddb68c2c06e
Amadeus <2270517836@qq.com>
2023-10-17 21:15:15 +0800

    重启

--- src/Java/ArraysSort/Base/QuickSort.java
+++ src/Java/ArraysSort/Base/QuickSort.java
@@ -16,6 +16,7 @@ public class QuickSort {
 
     //法1：覆盖法
     //特点是到最后 i一定等于j 也就是枢轴量应该在的位置
+    //输入的low、high都是索引，分布是 0 和 len-1
     static void sort1(int[] array, int low, int high) {
         if (low >= high)
         {
@@ -47,38 +48,39 @@ public class QuickSort {
 
     //法2：交换法
     //特点是 i、j循环到最后可能 j + 1 = i，也可能 i = j，代码简洁，但对比覆盖法，数组元素变动次数多了一些
+    //输入的low、high都是索引，分布是 0 和 len-1
     static void sort2(int[] array, int low, int high) {
         if (low >= high)
         {
             return;
         }
         //这里是取中位的下界位置为枢轴量，和取i为枢轴量差不多，不过更好，避免数组有序时时间复杂度到达O(n^2)。
+        //当分治边界取 j 和 j+1 时，pivot不能取 arr[high]，因为 j 下界为 low，则[j + 1, high]不会造成无限划分，但 [low, j]却可能造成，因为 j 可能取到 high。
         int pivot = array[(low + high) / 2];
         //初始化，arr[low, low+1, ..., i] <= pivot, arr[j, j+1, ..., high] >= pivot
         int i = low - 1, j = high + 1;
         while (i < j) {
-            //做完这个循环，arr[low, low+1, ..., i-1] < pivot, arr[i] >= pivot
+            //做完这个循环，arr[low, low+1, ..., i-1] <= pivot, arr[i] >= pivot
             do {
                 i++; //每次循环都要让i至少变化一次，不然如果arr[i]=pivot，就一直不动了
-            } while (array[i] < pivot);
-            //做完这个循环，arr[j+1, ..., high] > pivot, arr[j] <= pivot
+            } while (array[i] < pivot); //这里不能是<=，如果 arr 中元素全相等，则 i 会自增到 i+1，数组越界。
+            //做完这个循环，arr[j+1, ..., high] >= pivot, arr[j] <= pivot
             do {
                 j--;
             } while (array[j] > pivot);
             //以上前提可以保证 i - 1 < j + 1，即 i < j + 2。当整个循环结束后，有 i >= j，即存在 i = j 和 i = j + 1 两种情况，且i和j必不可能越界
-            //做完这个，则arr[i] <= pivot, arr[j] >= pivot，又回归到初始化的状态
-            if (i < j) {
+            //在最后一轮循环之前，做完下面这个if语句，arr[low, low+1, ..., i] <= pivot, arr[j, j+1, ..., high] >= pivot，又回归到初始化的状态
+            //但最后一轮有点特殊，因为这个if语句不会被执行，所以只能保证 arr[low, low+1, ..., i-1] <= pivot, arr[i] >= pivot, arr[j+1, ..., high] >= pivot, arr[j] <= pivot, i >= j
+            //所以 arr[low, low+1, ..., j] <= pivot, arr[j+1, ..., high] >= pivot, 此时arr[j, i] == pivot
+            if (i < j) {  // <= 也行，无所谓
                 swap(array, i, j);
             }
         }
-        //此时满足初始化时的状态，且i >= j，可推得arr[j, i] == pivot，arr[low, j] <= x，arr[j+1, high] >= x
         //要小心边界问题，以免造成无穷分治，即sort(0,n)划分为sort(0,0)和sort(0,n)
-        //j的取值范围是[low, high-1]，证明见下
-        //假设 j 最终的值为 high ，说明只有一轮循环(两轮的话 j 至少会自减两次)
+        //当pivot = arr[low + high >> 2]时，j的取值范围是[low, high-1]，证明见下
+        //假设 j 最终的值为 high，说明只有一轮循环(两轮的话 j 至少会自减两次)
         //说明arr[high] <= x (因为要跳出do-while循环)
-        //说明 i >= high(while循环的结束条件)， i 为 high 或 high + 1(必不可能成立)
-        //说明 i 自增到了 high , 说明 arr[high] >= pivot 和 q[low, ..., high-1] < pivot,
-        //得出 arr[high] = pivot 和 q[low, ..., high-1] < pivot 的结论,但这与 x = arr[low + high >> 1]矛盾。
+        //说明 i >= high (while循环的结束条件)， i 为 high 或 high + 1(必不可能成立)，说明 i 是**自增**到了 high，即 arr[low, ..., high-1] < pivot，arr[high] = pivot，但这与 pivot = arr[low + high >> 1]矛盾。
         sort2(array, low, j);
         sort2(array, j + 1, high);
         //若pivot取上界，则采用下面的分治边界
