aa295e24facf9388241d8b2eba1e0a6e9e98d73d
amadeus <a7858833@bupt.edu.cn>
2025-01-29 22:00:18 +0800

    LeetCode 03

---
+++ Python/Base/Hash/LongestConsecutive.py
@@ -0,0 +1,20 @@
+# LeetCode 03
+from typing import List
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        longest_streak = 0
+        num_set = set(nums) # dict和set是hash存储的 查找效率为o(1) tuple和list不是hash存储 查找效率为o(n)
+
+        for num in num_set:
+            if num - 1 not in num_set:
+                current_num = num
+                current_streak = 1
+
+                while current_num + 1 in num_set:
+                    current_num += 1
+                    current_streak += 1
+
+                longest_streak = max(longest_streak, current_streak)
+
+        return longest_streak
